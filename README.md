a2575293f68eeca658ff301f36ec32a04e93959a
Akbar Maliki <akbarmaliki109@gmail.com>
2024-02-26 20:14:34 +0700

    add crouch, dash, and space with changing texture on pressed

---
+++ README.md
@@ -0,0 +1,4 @@
+## Using Godot 3.5
+- Press `shift` to dash by simultaneously pressing `right arrow` or `left arrow` button
+- Press `space` to jump
+- Press `down arrow` to crouch
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -6,6 +6,17 @@ export (int) var jump_speed = -600
 
 const UP = Vector2(0,-1)
 
+const idle_texture: String = "res://assets/kenney_platformercharacters/PNG/Zombie/Poses/zombie_idle.png"
+const crouch_texture: String = "res://assets/kenney_platformercharacters/PNG/Zombie/Poses/zombie_duck.png"
+const walk_right_texture: String = "res://assets/kenney_platformercharacters/PNG/Zombie/Poses/zombie_walk1.png"
+const dash_right_texture: String = "res://assets/kenney_platformercharacters/PNG/Zombie/Poses/zombie_slide.png"
+const jump: String = "res://assets/kenney_platformercharacters/PNG/Zombie/Poses/zombie_jump.png"
+
+var jump_count: int = 0
+const max_jump = 2
+
+var can_dash: bool = false
+
 var velocity = Vector2()
 
 func _ready():
@@ -13,15 +24,50 @@ func _ready():
 
 func get_input():
 	velocity.x = 0
-	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
+	
+	if is_on_floor():
+		jump_count = 0
+	
+	if jump_count == 1 and Input.is_action_just_pressed("up"):
 		velocity.y = jump_speed
-	if Input.is_action_pressed('ui_right'):
+		$Sprite.texture = load(jump)
+		jump_count = 0
+	if is_on_floor() and Input.is_action_just_pressed("up"):
+		velocity.y = jump_speed
+		$Sprite.texture = load(jump)
+		jump_count += 1
+	if Input.is_action_just_released("up"):
+		$Sprite.texture = load(idle_texture)
+	
+	if Input.is_action_pressed('right'):
+		$Sprite.texture = load(walk_right_texture)
 		velocity.x += speed
-	if Input.is_action_pressed('ui_left'):
+	if Input.is_action_just_released("right"):
+		$Sprite.texture = load(idle_texture)
+
+	if Input.is_action_pressed('left'):
 		velocity.x -= speed
+
+	
+	# Dash
+	if Input.is_action_pressed('dash') and velocity.x > 0:
+		$Sprite.texture = load(dash_right_texture)
+		velocity.x += 400
+	if Input.is_action_pressed('dash') and velocity.x < 0:
+		velocity.x -= 400
+	if Input.is_action_just_released("dash"):
+		$Sprite.texture = load(idle_texture)
+	
+	# Crouch
+	if Input.is_action_pressed('down'):
+		velocity.y = 500
+		$Sprite.texture = load(crouch_texture)
+	if Input.is_action_just_released("down"):
+		$Sprite.texture = load(idle_texture)
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
 	get_input()
 	velocity = move_and_slide(velocity, UP)
+
 
